8dacc5e47791d9965f9f26ea0708f96f468fe333
Steven Lu <tacticalazn@gmail.com>
2018-02-26 02:34:16 -0500

    Replace fields from original email

--- mail_forward.js
+++ mail_forward.js
@@ -2,11 +2,22 @@ var AWS = require('aws-sdk');
 var ses = new AWS.SES();
 
 exports.handler = (event, context, callback) => {
+    var content = JSON.parse(event.Records[0].Sns.Message).content
+    
+    var from = content.match(/^from:.*\<(.*)\>/im)[1]
+    var subject = content.match(/^subject:(.*)$/im)[1].trim()
+
+    content = content.replace(/^from:.*\r\n/igm, '')
+    content = content.replace(/^reply-to:.*\r\n/igm, '')
+    content = content.replace(/^return-path:.*\r\n/igm, '')
+    content = content.replace(/^subject:.*\r\n/igm, 'Subject: [' + from + '] ' + subject + '\r\n')
+  
     ses.sendRawEmail({
       RawMessage: {
-        Data: JSON.parse(event.Records[0].Sns.Message).content
+        Data: content
       },
-      Destinations: [process.env.fwd_address],
+      Source: process.env.from_address,
+      Destinations: [process.env.to_address],
     }, function(err, data) {
       if (err) console.log(err, err.stack); // an error occurred
     });
