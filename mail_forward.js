388403c9fe9fe06044dc691283c3380cc7002a14
Anton Chernysh <acherny@softserveinc.com>
2018-03-19 12:22:45 +0200

    Updated Readme. Forward mail to multiple addresses.

--- mail_forward.js
+++ mail_forward.js
@@ -2,6 +2,7 @@ var AWS = require('aws-sdk');
 var ses = new AWS.SES();
 
 exports.handler = (event, context, callback) => {
+    var from = content.match(/^from:.*\<(.*)\>/im)[1]
     var content = JSON.parse(event.Records[0].Sns.Message).content;
     var subject = content.match(/^subject:(.*)$/im)[1].trim();
     var Destinations = process.env.to_address.split(",");
@@ -9,7 +10,7 @@ exports.handler = (event, context, callback) => {
     content = content.replace(/^from:.*\r\n/igm, '');
     content = content.replace(/^reply-to:.*\r\n/igm, '');
     content = content.replace(/^return-path:.*\r\n/igm, '');
-    content = content.replace(/^subject:.*\r\n/igm, 'Subject:' + subject + '\r\n');
+    content = content.replace(/^subject:.*\r\n/igm, 'Subject: [' + from + '] ' + subject + '\r\n')
         
     Destinations.forEach(function(element) {
       ses.sendRawEmail({
