35e7b6e3f0fceb9912fd47df0c9371cf1ce3652e
Simon Struck <simonstruck70@gmail.com>
2018-05-13 01:08:27 +0200

    Remove (personally) unnessacary subject changes
    Personally, I don't need the the modifications to the Subject line

--- mail_forward.js
+++ mail_forward.js
@@ -2,24 +2,20 @@ var AWS = require('aws-sdk');
 var ses = new AWS.SES();
 
 exports.handler = (event, context, callback) => {
-    var content = JSON.parse(event.Records[0].Sns.Message).content
-    
-    var from = content.match(/^from:.*\<(.*)\>/im)[1]
-    var subject = content.match(/^subject:(.*)$/im)[1].trim()
+  var content = JSON.parse(event.Records[0].Sns.Message).content;
 
-    content = content.replace(/^from:.*\r\n/igm, '')
-    content = content.replace(/^reply-to:.*\r\n/igm, '')
-    content = content.replace(/^return-path:.*\r\n/igm, '')
-    content = content.replace(/^subject:.*\r\n/igm, 'Subject: [' + from + '] ' + subject + '\r\n')
-    content = content.replace(/^DKIM-Signature: .*\r?\n(\s+.*\r?\n)*/mg, '')
+  content = content.replace(/^from:.*\r\n/igm, '');
+  content = content.replace(/^reply-to:.*\r\n/igm, '');
+  content = content.replace(/^return-path:.*\r\n/igm, '');
+  content = content.replace(/^DKIM-Signature: .*\r?\n(\s+.*\r?\n)*/mg, '');
 
-    ses.sendRawEmail({
-      RawMessage: {
-        Data: content
-      },
-      Source: process.env.from_address,
-      Destinations: [process.env.to_address],
-    }, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-    });
+  ses.sendRawEmail({
+    RawMessage: {
+      Data: content
+    },
+    Source: process.env.from_address,
+    Destinations: [process.env.to_address],
+  }, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+  });
 }
