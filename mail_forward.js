fa71803ed8c892013193a7408cc0155f4c694d07
Simon Struck <simonstruck70@gmail.com>
2018-06-01 21:46:46 +0200

    Fix receiving email from unknown addresses
    Previous implementation only worked when sending test-messages to myself, so not very useful

--- mail_forward.js
+++ mail_forward.js
@@ -2,13 +2,24 @@ var AWS = require('aws-sdk');
 var ses = new AWS.SES();
 
 exports.handler = (event, context, callback) => {
-  ses.sendRawEmail({
-    RawMessage: {
-      Data: JSON.parse(event.Records[0].Sns.Message).content.replace(/^DKIM-Signature: .*\r?\n(\s+.*\r?\n)*/mg, '')
-    },
-    Source: process.env.from_address,
-    Destinations: [process.env.to_address],
-  }, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-  });
-}
+    var content = JSON.parse(event.Records[0].Sns.Message).content;
+
+    var from = content.match(/^from:(.*)\<(.*)\>/im);
+    var to = content.match(/^to:.*\<(.*)\>/im)[1];
+
+    content = content.replace(/^from:.*\r\n/igm, 'From: "' + from[2] + '" <' + to +'>\r\n');
+    content = content.replace(/^to:.*\r\n/igm, '');
+    content = content.replace(/^reply-to:.*\r\n/igm, '');
+    content = content.replace(/^return-path:.*\r\n/igm, '');
+    content = content.replace(/^DKIM-Signature: .*\r?\n(\s+.*\r?\n)*/mg, '');
+
+    ses.sendRawEmail({
+      RawMessage: {
+        Data: content
+      },
+      Source: to,
+      Destinations: [process.env.to_address],
+    }, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+    });
+};
