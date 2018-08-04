05da18caf9a89543fdbec16918162b407e378e2c
Matej Šircelj <sircelj.m@gmail.com>
2018-08-04 14:56:53 +0200

    update mail forward lambda

--- mail_forward.js
+++ mail_forward.js
@@ -1,24 +1,33 @@
-var AWS = require('aws-sdk');
+
+var AWS = require("aws-sdk");
 var ses = new AWS.SES();
 
 exports.handler = (event, context, callback) => {
-    var content = JSON.parse(event.Records[0].Sns.Message).content
-    
-    var from = content.match(/^from:.*\<(.*)\>/im)[1]
-    var subject = content.match(/^subject:(.*)$/im)[1].trim()
+    let data = JSON.parse(event.Records[0].Sns.Message);
+    let source = data.content;
 
-    content = content.replace(/^from:.*\r\n/igm, '')
-    content = content.replace(/^reply-to:.*\r\n/igm, '')
-    content = content.replace(/^return-path:.*\r\n/igm, '')
-    content = content.replace(/^subject:.*\r\n/igm, 'Subject: [' + from + '] ' + subject + '\r\n')
-  
-    ses.sendRawEmail({
-      RawMessage: {
-        Data: content
-      },
-      Source: process.env.from_address,
-      Destinations: [process.env.to_address],
-    }, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-    });
-}
+    source = source.replace(/^DKIM-Signature/im, "X-Original-DKIM-Signature");
+    source = source.replace(/^From/im, "X-Original-From");
+    source = source.replace(/^Source/im, "X-Original-Source");
+    source = source.replace(/^Sender/im, "X-Original-Sender");
+    source = source.replace(/^Return-Path/im, "X-Original-Return-Path");
+    source = source.replace(/^Domainkey-Signature/im, "X-Original-Domainkey-Signature");
+    source = `From: AWS SES Mail Forwarding <${data.mail.destination}>\n` + source;
+
+    if(!process.env.to_address) {
+        console.log("You need to supply to_address enviromnent variable");
+    } else if(data.mail.destination == process.env.to_address) {
+        console.log("MAIL SEND ABORTED! - Forwarded mail address and to_address are the same - THIS CAN CAUSE AN INFINITE LOOP!");
+    } else {
+        ses.sendRawEmail({
+            RawMessage: {
+                Data: source,
+            },
+            Destinations: [process.env.to_address],
+        }, function (err, data) {
+            if (err) {
+                console.error(err, err.stack);
+            }
+        });
+    }
+};
